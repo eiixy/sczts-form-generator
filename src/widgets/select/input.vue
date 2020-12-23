4d96fe148676a14ffd4677a45cd54b8b9828c0be
eiixy <990656271@qq.com>
2020-12-23 14:33:22 +0800

    修复单选框只读配置不生效的问题

--- src/widgets/select/input.vue
+++ src/widgets/select/input.vue
@@ -3,8 +3,8 @@
     class="w100"
     v-model="item.value"
     :placeholder="item.attributes.placeholder"
+    :readonly="'readonly' in item.attributes ? item.attributes.readonly : false"
   >
-  :readonly="'readonly' in item.attributes ? item.attributes.readonly : false"
     <el-option
       v-for="(option,i) in item.attributes.options"
       :key="i"
